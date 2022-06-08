b6a73f38e6e83be1ef6e225868667b37d1621bbd
Vsevolod Bezaev <vvbezaev@gmail.com>
2022-06-08 18:21:50 +0300

    Initial configuration

---
+++ backend/src/test/java/com/sev4ikwasd/passwordmanager/PasswordManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sev4ikwasd.passwordmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
